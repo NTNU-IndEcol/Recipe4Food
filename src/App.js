e4bb257e2bca3135de62353783403bdd267c6df1
BoHuang <huangb@live.com>
2024-10-22 09:53:52 +0200

    extract recipe 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+//import recipes from './data/recipes.json';
+//import categories from './data/categories.json';
 import RecipeDetail from './pages/RecipeDetail';
 
 function App() {
--- src/extract_recipe_4one.py
+++ src/extract_recipe_4one.py
@@ -2,38 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-def get_recipe_links(category_url):
-    recipe_links = []
-    page = 1
-
-    while True:
-        # Construct the URL for each page
-        url = f"{category_url}/page/{page}/"
-        print(f"Scraping page: {url}")
-        
-        # Fetch the category page content
-        response = requests.get(url)
-        if response.status_code != 200:
-            print(f"No more pages found or failed to retrieve the URL: {url}")
-            break
-        
-        # Parse the category page
-        soup = BeautifulSoup(response.content, 'html.parser')
-        
-        # Find all links to individual recipes
-        found_links = soup.select('h2.entry-title a')
-        if not found_links:
-            print(f"No more recipes found on page {page}")
-            break
-        
-        # Add links to the list
-        for a_tag in found_links:
-            recipe_links.append(a_tag['href'])
-        
-        page += 1  # Move to the next page
-    
-    return recipe_links
-
 def extract_recipe_details(url):
     # Fetch the webpage content
     response = requests.get(url)
@@ -86,18 +54,9 @@ def save_to_json(data, file_name):
     print(f"Recipe data saved to {file_name}")
 
 if __name__ == "__main__":
-    # Base URL of the "Appetizer/Snack" category page
-    category_url = "https://www.koreanbapsang.com/category/appetizersnack"
+    # Replace with any recipe page URL
+    url = "https://www.koreanbapsang.com/dubu-salad-korean-tofu-salad/"
     
-    # Get all recipe links from all pages of the category
-    recipe_links = get_recipe_links(category_url)
-    
-    all_recipes = []
-    for link in recipe_links:
-        print(f"Extracting recipe from {link}")
-        recipe_data = extract_recipe_details(link)
-        if recipe_data:
-            all_recipes.append(recipe_data)
-    
-    # Save all the recipes in a single JSON file
-    save_to_json(all_recipes, 'all_appetizer_snack_recipes.json')
+    recipe_data = extract_recipe_details(url)
+    if recipe_data:
+        save_to_json(recipe_data, 'recipe_data.json')
